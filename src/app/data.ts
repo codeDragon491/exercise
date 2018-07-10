4eea9567f486c71d312077eac74a6405f7fbbb45
Julia Trefas <hola@juliatrefas.com>
2018-07-10 19:55:48 +0300

    Update and rename data.json to data.ts

---
+++ src/app/data.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { of } from 'rxjs/observable/of';
+
+@Injectable()
+export class Data {
+
+    [x: string]: any;
+
+    person = [{
+        response: {
+            val1: 1,
+            val2: 2
+        }
+    }];
+    facility = [
+        { val1: 1 }, {
+            response: {
+                val3: 3,
+                val4: 4
+            }
+        }];
+    exposure = [
+        { val2: 2 }, {
+            response: {
+                val5: 5
+            }
+        }]
+
+    constructor() { }
+
+    public createPerson(arg) {
+        this.person.push(arg)
+        return of(this.person[0].response)
+    }
+    public getFacility(val1: Number) {
+        this.facility.find(x => x.val1 === val1);
+        return of(this.facility[1].response);
+    }
+    public getExposure(val2: Number) {
+        this.exposure.find(x => x.val2 === val2)
+        return of(this.exposure[1].response);
+    }
+}
