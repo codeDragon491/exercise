ed311630e4580bc3d9531b54776663d4ef199d83
Julia Trefas <hola@juliatrefas.com>
2018-07-10 10:45:26 +0300

    Update data.service.ts

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,28 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/observable/forkJoin';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class DataService {
-  person;
-  facility;
+  
   apiUrl1: string = 'http://localhost:3000/person/?';
   apiUrl2: string = 'http://localhost:3000/facility/?';
   apiUrl3: string = 'http://localhost:3000/exposure/?';
 
   constructor(private http: HttpClient) { }
 
-  getData(arg): Observable<any> {
+   getData(arg): Observable<any> {
     return this.http.get(this.apiUrl1 + arg.data)
-      .map((response: any) =>
-        this.person = response
-      )
-      .flatMap((response: any) => this.http.get(this.apiUrl2 + response.val1)
-        .map((response: any) => {
-          this.facility = response
-        })
-        .flatMap(() => this.http.get(this.apiUrl3 + this.person.val2)
-          .map((response: any) => response.val5 * this.facility.val3)))
+      .flatMap((person: any) => Observable.forkJoin(this.http.get(this.apiUrl2 + person.val1), this.http.get(this.apiUrl3 + person.val2)))
+      .map(([facility, exposure]) => facility.val3 * exposure.val5)
   }
 }
